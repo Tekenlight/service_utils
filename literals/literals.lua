c3b7e36d804bd5f24a6896025488d46e78c8ba0f
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2022-05-30 14:39:14 +0530

    Fixed defect in literals.lua

--- literals/literals.lua
+++ literals/literals.lua
@@ -1,6 +1,6 @@
 local msf = require('service_utils.common.msg_literal');
 local literals = {
-	,RECORD_NOT_FOUND = "Record not found for: [%s]"
+	RECORD_NOT_FOUND = "Record not found for: [%s]"
 	,DUPLICATE_RECORD_FOUND = "Duplicate record found for : [%s]"
 };
 
