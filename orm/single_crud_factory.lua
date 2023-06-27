62a218f33905a8445ae9f488b1ae5088701b69cb
amit <tekenlight_amit@cashrepublic.com>
2023-06-27 13:00:01 +0530

    modified approve, select after approve

--- orm/single_crud_factory.lua
+++ orm/single_crud_factory.lua
@@ -205,7 +205,25 @@ single_crud.approve = function (self, context, obj)
 		end
 	end
 
-	return true, nil, ret;
+	local key_params, key_count = get_key_params(tao, obj);
+	local out, msg = tao:select(context, table.unpack(key_params));
+
+	if (out == nil) then
+		local key_param_str = get_key_params_str(tao, obj);
+		local msg = messages:format('RECORD_NOT_FOUND', key_param_str);
+		error_handler.raise_error(404, msg);
+		return false, nil;
+	end
+
+	-- remove creation_uid, creation_time, update_uid, update_time from the table
+	if out ~= nil and out.creation_uid ~= nil then out.creation_uid = nil end
+	if out ~= nil and out.creation_time ~= nil then out.creation_time = nil end
+	if out ~= nil and out.update_uid ~= nil then out.update_uid = nil end
+	if out ~= nil and out.update_time ~= nil then out.update_time = nil end
+
+	local obj = mapper.copy_elements(self.msg_ns, self.msg_elem_name, out);
+
+	return true, obj, ret;
 end
 
 single_crud.cancel = function (self, context, obj)
