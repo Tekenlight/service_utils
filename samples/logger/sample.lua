6414c789d33059f12d295af1cafe3f1096a4fdc1
amit <tekenlight_amit@cashrepublic.com>
2023-08-04 19:24:14 +0530

    updated sample with required information

--- samples/logger/sample.lua
+++ samples/logger/sample.lua
@@ -1,5 +1,11 @@
-local log = require('evlualogger');
+-- Entries required in properties file to change the behaviour
+-- logging.level = OFF
+-- logging.file_location = /Users/v3nom/workspace/tekenlight/product/service_utils/logging
+-- logging.file_name = evlua
+-- logging.log_to_console = false
+-- logging.log_to_file = false
 
+local log = require('evlualogger');
 local some_func = function ()
     log:debug("try");
     local context = { user_id = 123, ip_address = "192.168.1.1", uuid = require('service_utils.common.utils').uuid() }
@@ -20,7 +26,6 @@ local some_func = function ()
 end
 
 some_func()
-
 log:debug("try");
 local context = { user_id = 123, ip_address = "192.168.2.1", uuid = require('service_utils.common.utils').uuid() }
 log:set_context(context, "user_id", "ip_address")
