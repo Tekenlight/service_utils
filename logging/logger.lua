feaf30aff041ef78664bdf55e8e6914ada8dc6ae
amit <tekenlight_amit@cashrepublic.com>
2023-08-08 11:36:34 +0530

    added rolling file logic.

--- logging/logger.lua
+++ logging/logger.lua
@@ -1,5 +1,16 @@
 local logger = {};
 
+local function rollLogFile(maxBackupFiles, logFileName)
+    local fileNum = maxBackupFiles - 1
+    while fileNum > 0 do
+        local currentFile = string.format("%s.%d", logFileName, fileNum)
+        local nextFile = string.format("%s.%d", logFileName, fileNum + 1)
+        os.rename(currentFile, nextFile)
+        fileNum = fileNum - 1
+    end
+    os.rename(logFileName, logFileName .. ".1")
+end
+
 logger.init = function(self, context, ...)
     local properties_funcs = platform.properties_funcs();
     local fileLocation = properties_funcs.get_string_property("logging.file_location") or os.getenv("EVLUA_PATH");
@@ -7,6 +18,8 @@ logger.init = function(self, context, ...)
     local fileName = properties_funcs.get_string_property("logging.file_name") or "evlua";
     local logToConsole = properties_funcs.get_bool_property("logging.log_to_console") or false;
     local logToFile = properties_funcs.get_bool_property("logging.log_to_file") or false;
+    local maxBackupFile = properties_funcs.get_int_property("logging.max_backup_file") or 1;
+    local maxFileSize = (properties_funcs.get_int_property("logging.max_file_size") or 1) * 1024;
 
     context = context or {};
     local contextFieldToBeLogged = { ... };
@@ -34,6 +47,12 @@ logger.init = function(self, context, ...)
             if file then
                 file:write(formattedMessage .. "\n")
                 file:close()
+            end
+
+            -- Check if the file size exceeds the maximum
+            local filesize = io.open(fileLocation .. "/" .. fileName .. ".log", "r"):seek("end")
+            if filesize >= maxFileSize then
+                rollLogFile(maxBackupFile, fileLocation .. "/" .. fileName .. ".log")
             end
         end
     end)
